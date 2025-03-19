94ed566acc59d0e3962bf6e9ac6a8935c122f066
suzykang <pc_suzi@daum.net>
2025-03-19 15:24:08 +0900

    chore: spring boot 기본 셋팅 파일 추가

---
+++ src/test/java/com/hello/__suzy_kang_community_be/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.__suzy_kang_community_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

19b2976e0a0120d3953cd738a02c2b7e4ec77721
suzykang <pc_suzi@daum.net>
2025-03-19 15:41:48 +0900

    feat: 유저 엔티티 구현

---
+++ src/main/java/com/hello/__suzy_kang_community_be/user/entity/User.java
@@ -0,0 +1,49 @@
+package com.hello.__suzy_kang_community_be.user.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@Getter @Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+
+    @Column(nullable = false, length = 40, unique = true)
+    private String email;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String profile;
+
+    @Column(nullable = false, length = 40)
+    private String password;
+
+    @Column(nullable = false, length = 10)
+    private String nickname;
+
+    @Builder
+    public User(String email, String profile, String password, String nickname) {
+        this.email = email;
+        this.profile = profile;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+
+}

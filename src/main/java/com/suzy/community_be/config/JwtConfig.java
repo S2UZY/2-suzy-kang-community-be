14665f361b05431e993349351760402c8026bef4
suzykang <pc_suzi@daum.net>
2025-03-25 16:45:04 +0900

    feat: JwtConfig 클래스 추가

---
+++ src/main/java/com/suzy/community_be/config/JwtConfig.java
@@ -0,0 +1,15 @@
+package com.suzy.community_be.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class JwtConfig {
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long expirationTime;
+}

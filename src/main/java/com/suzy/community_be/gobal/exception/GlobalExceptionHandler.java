f3b50f66111f6c6b7e8a3f351e45220feb8a4d94
suzykang <pc_suzi@daum.net>
2025-03-25 16:50:42 +0900

    chore: 에러코드 더 상세하게 보도록 AOP 개선

--- src/main/java/com/suzy/community_be/gobal/exception/GlobalExceptionHandler.java
+++ src/main/java/com/suzy/community_be/gobal/exception/GlobalExceptionHandler.java
@@ -2,12 +2,17 @@ package com.suzy.community_be.gobal.exception;
 
 
 import com.suzy.community_be.gobal.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
@@ -19,8 +24,18 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse> handleException(Exception e) {
+        String errorLog = getStackTraceAsString(e);
+
+        log.error("[ERROR] : {}\n{}", e.getMessage(), errorLog);
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body(new ApiResponse("internal_server_error", false,null));
+    }
+
+    private String getStackTraceAsString(Throwable e) {
+        StringWriter stringWriter = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(stringWriter);
+        e.printStackTrace(printWriter);
+        return stringWriter.toString();
     }
 }

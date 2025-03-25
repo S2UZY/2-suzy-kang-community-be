9c1b3cfa84fb9f545b535905257c77992efa58d4
suzykang <pc_suzi@daum.net>
2025-03-25 16:59:44 +0900

    feat: Jwt 필터 추가

---
+++ src/main/java/com/suzy/community_be/security/JwtAuthenticationFilter.java
@@ -0,0 +1,44 @@
+package com.suzy.community_be.security;
+
+import com.suzy.community_be.gobal.utils.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizationHeader = request.getHeader("Authorization");
+
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            String token = authorizationHeader.substring(7);
+            try{
+                Long userId = jwtUtil.extractUserId(token);
+                request.setAttribute("userId",userId);
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userId, null, Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))
+                );
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (Exception e){
+                System.out.println("JWT 인증 실패: " + e.getMessage());
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+}

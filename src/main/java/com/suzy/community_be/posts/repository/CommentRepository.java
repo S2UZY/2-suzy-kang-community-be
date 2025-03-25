7ccfe52e5679769845346cd80c2840db4289a3b6
suzykang <pc_suzi@daum.net>
2025-03-25 17:16:23 +0900

    feat: 댓글 레퍼지토리 인터페이스 정의

---
+++ src/main/java/com/suzy/community_be/posts/repository/CommentRepository.java
@@ -0,0 +1,12 @@
+package com.suzy.community_be.posts.repository;
+
+import com.suzy.community_be.posts.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    Optional<Comment> findByIdAndPostId(Long id, Long postId);
+} 

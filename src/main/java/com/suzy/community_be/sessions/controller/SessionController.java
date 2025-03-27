e322e27ed23877a7eaaf55f112b276af35eb675f
suzykang <pc_suzi@daum.net>
2025-03-27 14:25:57 +0900

    feat: 로그인 로그아웃 쿠키 로직 추가, 로그인 유무 API 추가

--- src/main/java/com/suzy/community_be/sessions/controller/SessionController.java
+++ src/main/java/com/suzy/community_be/sessions/controller/SessionController.java
@@ -4,9 +4,14 @@ import com.suzy.community_be.gobal.response.ApiResponse;
 import com.suzy.community_be.sessions.dto.request.LoginRequestDto;
 import com.suzy.community_be.sessions.dto.response.LoginResponseDto;
 import com.suzy.community_be.sessions.service.SessionService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/sessions")
@@ -15,14 +20,24 @@ public class SessionController {
     private final SessionService sessionService;
 
     @PostMapping
-    public ResponseEntity<ApiResponse> login(@RequestBody LoginRequestDto requestDto){
-        LoginResponseDto responseDto = sessionService.login(requestDto);
+    public ResponseEntity<ApiResponse> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response){
+        LoginResponseDto responseDto = sessionService.login(requestDto, response);
         return ResponseEntity.ok(new ApiResponse("login_success",true,responseDto));
     }
 
     @DeleteMapping
-    public ResponseEntity<ApiResponse> logout(){
-        sessionService.logout();
+    public ResponseEntity<ApiResponse> logout(HttpServletResponse response){
+        sessionService.logout(response);
         return ResponseEntity.ok(new ApiResponse("logout_success", true,null));
     }
+
+    @GetMapping("/check")
+    public ResponseEntity<ApiResponse> checkLogin(HttpServletRequest request){
+        boolean isLoggedIn = sessionService.checkLogin(request);
+        Map<String, Boolean> result = new HashMap<>();
+        result.put("isLoggedIn", isLoggedIn);
+
+        return ResponseEntity.ok(new ApiResponse("login_check_success", true, result));
+    }
+
 }

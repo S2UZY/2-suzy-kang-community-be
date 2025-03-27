59280e08b69816cb728d8cdde87f29dc37dbc0f9
suzykang <pc_suzi@daum.net>
2025-03-27 14:26:36 +0900

    feat: httpOnly 쿠키를 사용해서 jwt 로그인 로그아웃으로 서비스 변경

--- src/main/java/com/suzy/community_be/sessions/service/SessionService.java
+++ src/main/java/com/suzy/community_be/sessions/service/SessionService.java
@@ -7,7 +7,10 @@ import com.suzy.community_be.sessions.dto.request.LoginRequestDto;
 import com.suzy.community_be.sessions.dto.response.LoginResponseDto;
 import com.suzy.community_be.users.entity.User;
 import com.suzy.community_be.users.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,7 +19,7 @@ public class SessionService {
     private final UserRepository userRepository;
     private final JwtUtil jwtUtil;
 
-    public LoginResponseDto login(LoginRequestDto requestDto){
+    public LoginResponseDto login(LoginRequestDto requestDto, HttpServletResponse response){
         User user = userRepository.findByEmail(requestDto.getEmail())
                 .orElseThrow(()->new CustomException(ErrorCode.USER_NOT_FOUND));
 
@@ -25,10 +28,27 @@ public class SessionService {
         }
 
         String token = jwtUtil.generateToken(user.getId());
-        return new LoginResponseDto(token);
+        jwtUtil.addTokenToCookie(response, token);
+
+        return new LoginResponseDto(user.getId(), user.getEmail(), user.getNickname(), user.getProfile());
     }
 
-    public void logout(){
+    public void logout(HttpServletResponse response){
+        jwtUtil.deleteCookie(response);
+        SecurityContextHolder.clearContext();
+    }
 
+    public boolean checkLogin(HttpServletRequest request){
+        String token = jwtUtil.extractTokenFromCookie(request);
+        if(token == null){
+            return false;
+        }
+
+        try{
+            Long userId = jwtUtil.extractUserId(token);
+            return !jwtUtil.isTokenExpired(token) && userRepository.existsById(userId);
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
